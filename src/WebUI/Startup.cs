48a8c9ded81087c5292c5e03a2fffb096a05bce8
Amadeusz Drożdż <drozdz.amadeusz@gmail.com>
2022-04-09 18:19:26 +0200

    Update Startup.cs

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -136,7 +136,7 @@ namespace talker.WebUI
             }
 
             app.UseHealthChecks("/health");
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
